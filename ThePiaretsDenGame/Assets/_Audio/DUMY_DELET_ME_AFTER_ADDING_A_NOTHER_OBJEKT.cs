5f5818bcf0e0b01ab434aec44575209de07165e9
Franz <a17frajo@student.his.se>
2019-04-08 13:18:43 +0200

    Start version
    Lade in fungus och organiserade lite mer i fil strukturen.

---
+++ ThePiaretsDenGame/Assets/_Audio/DUMY_DELET_ME_AFTER_ADDING_A_NOTHER_OBJEKT.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DUMY_DELET_ME_AFTER_ADDING_A_NOTHER_OBJEKT : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
