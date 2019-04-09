681b1d6d5f8b1ab3a5324b2ac2fba791ed80801e
Franz <a17frajo@student.his.se>
2019-04-09 15:54:50 +0200

    ConditionalReaction
    La till reaktione som sätter conditions

---
+++ ThePiaretsDenGame/Assets/_Scripts/Editor/Interaction/ReactionEditors/EDI_ConditionReaction.cs
@@ -0,0 +1,55 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(SOBJ_ConditionReaction))]
+public class EDI_ConditionReaction : EDI_Reaction
+{
+    private SerializedProperty conditionProperty;       // Represents the Condition that will be changed.
+    private SerializedProperty satisfiedProperty;       // Represents the value that the Condition's satifised flag will be set to.
+
+    // Name of the field which is the Condition that will be changed.
+    private const string conditionReactionPropConditionName = "condition";
+    // Name of the bool field which is the value the Condition will get.
+    private const string conditionReactionPropSatisfiedName = "satisfied";
+
+
+
+    protected override void Init()
+    {
+        // Cache the SerializedProperties.
+        conditionProperty = serializedObject.FindProperty(conditionReactionPropConditionName);
+        satisfiedProperty = serializedObject.FindProperty(conditionReactionPropSatisfiedName);
+    }
+
+
+    protected override void DrawReaction()
+    {
+        /* If there's isn't a Condition yet, set it to the first 
+         * Condition from the AllConditions array.
+         */ 
+        if (conditionProperty.objectReferenceValue == null)
+        {
+            conditionProperty.objectReferenceValue = EDI_AllConditions.TryGetConditionAt(0);
+        }
+
+
+        // Get the index of the Condition in the AllConditions array.
+        int index = EDI_AllConditions.TryGetConditionIndex((SOBJ_Condition)conditionProperty.objectReferenceValue);
+
+        /* Use and set that index based on a popup of 
+         * all the descriptions of the Conditions.
+         */ 
+        index = EditorGUILayout.Popup(index, EDI_AllConditions.AllConditionDescriptions);
+
+        // Set the Condition based on the new index from the AllConditions array.
+        conditionProperty.objectReferenceValue = EDI_AllConditions.TryGetConditionAt(index);
+
+        // Use default toggle GUI for the satisfied field.
+        EditorGUILayout.PropertyField(satisfiedProperty);
+    }
+
+
+    protected override string GetFoldoutLabel()
+    {
+        return "Condition Reaction";
+    }
+}
---
+++ ThePiaretsDenGame/Assets/_Scripts/ScriptableObjects/Interaction/Reactions/ImmediateReactions/SOBJ_ConditionReaction.cs
@@ -0,0 +1,20 @@
+﻿/* This is the Reaction to change the satisfied state
+*  of a Condition.  The Condition here is a reference
+*  to one on the AllConditions asset.  That means by
+*  changing the Condition here, the global game
+*  Condition will change.  Since Reaction decisions
+*  are made based on Conditions, the change must be
+*  immediate and therefore this is a Reaction rather
+*  than a DelayedReaction.
+*/
+public class SOBJ_ConditionReaction : SOBJ_Reaction
+{
+    public SOBJ_Condition condition;// The SOBJ_Condition to be changed.
+    public bool satisfied;          // The satisfied state the SOBJ_Condition will be changed to.
+
+
+    protected override void ImmediateReaction()
+    {
+        condition.satisfied = satisfied;
+    }
+}
