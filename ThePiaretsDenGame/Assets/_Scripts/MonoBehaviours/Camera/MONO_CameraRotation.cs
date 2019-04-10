6805c093d56764519586bc940c4e92a8411133f9
Sebastian <sebbe.carlsson@gmail.com>
2019-04-10 10:35:48 +0200

    Alternative camera functionality
    Now allows the use of camera points and the smooth transition between them.

--- ThePiaretsDenGame/Assets/_Scripts/MonoBehaviours/Camera/MONO_CameraRotation.cs
+++ ThePiaretsDenGame/Assets/_Scripts/MonoBehaviours/Camera/MONO_CameraRotation.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 public class MONO_CameraRotation : MonoBehaviour
 {
 	public bool moveCamera 		= true;                     	// Whether the camera should be moved by this script.    
-	public float smoothing 		= 7f;                       	// Smoothing applied during Slerp, higher is smoother but slower.
-	public Vector3 offset 		= new Vector3 (0f, 1.5f, 0f); 	// The offset from the player's position that the camera aims at.
+	public float smoothing 		= 200f;                       	// Smoothing applied during Slerp, higher is smoother but slower.
+	public float currentDist;
 	public Transform playerPosition;                    		// Reference to the player's Transform to aim at.
+	public GameObject firstPoint;
+	public GameObject[] camPoints;
 
+	private GameObject currentPoint;
 
-	private IEnumerator Start ()
+	private IEnumerator Start()
 	{
 		// If the camera shouldn't move, do nothing.
 		if(!moveCamera)
@@ -18,30 +21,38 @@ public class MONO_CameraRotation : MonoBehaviour
 		// Wait a single frame to ensure all other Starts are called first.
 		yield return null;
 
-		// Set the rotation of the camera to look at the player's position with a given offset.
-		transform.rotation = Quaternion.LookRotation(playerPosition.position - transform.position + offset);
+		//Set camera to first point
+		currentPoint = firstPoint;
+		currentDist = Vector3.Distance (firstPoint.transform.position, transform.position);
 	}
 
 
-	// LateUpdate is used so that all position updates have happened before the camera aims.
-	private void LateUpdate ()
+	private void LateUpdate()
 	{
-		// If the camer shouldn't move, do nothing.
+		// If the camera shouldn't move, do nothing.
 		if (!moveCamera)
 		{
 			return;
 		}
 
-		 
+		//Update distance to compare other points with 
+		currentDist = Vector3.Distance (currentPoint.transform.position, playerPosition.position);
+
+		//Find closest point by comparing distances between current point and all points.
+		for(int i = 0; i < camPoints.Length; i++)
+		{
+			float newDist = Vector3.Distance (camPoints[i].transform.position, playerPosition.position);
+			if(newDist < currentDist)
+			{
+				currentPoint = camPoints [i];
+			}
+		}
 
 		// Find a new rotation aimed at the player's position with a given offset.
-		Quaternion newRotation = Quaternion.LookRotation (playerPosition.position - transform.position + offset);
-		newRotation = new Quaternion (newRotation.x, 0, 0, 1);
+		Quaternion newRotation = Quaternion.LookRotation (currentPoint.transform.position - transform.position);
 
-
-		// Spherically interpolate between the camera's current rotation and the new rotation.
+		//Move towards closest point.
 		transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, Time.deltaTime * smoothing);
-
-
 	}
 }
+	
