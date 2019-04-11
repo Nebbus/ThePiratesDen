58f4417e1f44538a1ab98f5c6b69b5e98f7861a0
Sebastian <sebbe.carlsson@gmail.com>
2019-04-11 17:09:57 +0200

    End of day
    Started funguswork

---
+++ ThePiaretsDenGame/Assets/_Scripts/MonoBehaviours/Interaction/MONO_DialogInit_Fungus.cs
@@ -0,0 +1,19 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+
+	/// <summary>
+	/// The block will execute when an object is interacted with.
+	/// </summary>
+	[EventHandlerInfo("Reactions",
+		"OnInteract",
+		"The block will execute when an object is interacted with.")]
+	[AddComponentMenu("")]
+	public class MONO_DialogInit_Fungus : EventHandler
+	{
+		protected virtual void DialogInit()
+		{
+			ExecuteBlock ();
+		}
+	}}
