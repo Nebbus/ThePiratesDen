a439fdb1a514c8ba5f66514187c34124a73dc677
Franz <a17frajo@student.his.se>
2019-04-15 11:03:34 +0200

    Implemented footstep based loop and animaton

---
+++ ThePiaretsDenGame/Assets/_Scripts/MonoBehaviours/Player/MONO_PlayerFootStepPllayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class MONO_PlayerFootStepPllayer : MonoBehaviour {
+
+
+    [FMODUnity.EventRef]
+    public string pathToFootStep;
+
+
+    /// <summary>
+    /// This funktion will be calld by a animations event,
+    /// so it ned to be events when the players fot hits the ground.
+    /// </summary>
+    private void Step()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(pathToFootStep);
+    }
+}
