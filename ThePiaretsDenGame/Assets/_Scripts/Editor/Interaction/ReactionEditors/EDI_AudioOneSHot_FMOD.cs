b5213d8b7012880cbdd671628433528a2241fb36
Franz <a17frajo@student.his.se>
2019-04-15 08:29:00 +0200

    one shot play rection added
    La in en On shot player reaktion samt flyttades döptes musik test skriptet om till namn staderden och flyttades till rätt ställe i map hearkin

---
+++ ThePiaretsDenGame/Assets/_Scripts/Editor/Interaction/ReactionEditors/EDI_AudioOneSHot_FMOD.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SOBJ_AudioOneSHot_FMOD))]
+public class EDI_AudioOneSHot_FMOD : EDI_Reaction
+{
+    protected override string GetFoldoutLabel()
+    {
+        return "SOBJ_AudioOneSHot_FMOD";
+    }
+
+}
