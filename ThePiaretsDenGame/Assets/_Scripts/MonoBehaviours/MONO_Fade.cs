09a5c7d418572571d457fad3e4ac33cf6962a4f3
Sebastian <sebbe.carlsson@gmail.com>
2019-04-15 12:06:13 +0200

    Fade
    lunch

---
+++ ThePiaretsDenGame/Assets/_Scripts/MonoBehaviours/MONO_Fade.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MONO_Fade : MonoBehaviour 
+{
+	public int fadeDuration;
+	public bool fadeOut;
+	public CanvasGroup canvas;
+
+	private float finalAlpha;
+
+	public void Fade()
+	{
+		StartCoroutine (DoFade ());
+
+	}
+
+	IEnumerator DoFade()
+	{
+		if (fadeOut)
+		{
+			finalAlpha = 1f;
+			while (canvas.alpha < finalAlpha) 
+			{
+				canvas.alpha += Time.deltaTime / fadeDuration;
+				yield return null;
+			}
+		} 
+		else 
+		{
+			finalAlpha = 0f;
+			while (canvas.alpha > finalAlpha) 
+			{
+				canvas.alpha += Time.deltaTime / fadeDuration;
+				yield return null;
+			}
+		}
+	}
+}
