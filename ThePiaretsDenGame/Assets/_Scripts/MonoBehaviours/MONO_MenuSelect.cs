daffab96b9d3d8b555e2cc33922fe19ee5b411c7
Sara <sara.uvalic@hotmail.com>
2019-04-23 17:07:54 +0200

    some menu stuff

---
+++ ThePiaretsDenGame/Assets/_Scripts/MonoBehaviours/MONO_MenuSelect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class MONO_MenuSelect : MonoBehaviour {
+
+	public EventSystem eventSystem;
+
+	[Tooltip("First button on panel")]
+	public GameObject selectedObject;	
+
+	private bool buttonSelected;
+
+
+
+	void Update () {
+		//selects a button depending on the up and down arrow
+		if (Input.GetAxisRaw ("Vertical") != 0 && !buttonSelected) 
+		{
+			eventSystem.SetSelectedGameObject (selectedObject);
+			buttonSelected = true;
+		}
+		else if(eventSystem.IsPointerOverGameObject ())
+		{
+			eventSystem.SetSelectedGameObject (null);
+			buttonSelected = false;
+		}
+	}
+
+
+	private void OnDisable()
+	{
+		eventSystem.SetSelectedGameObject (null);
+		buttonSelected = false;		//resets the menu so that no button is selected
+	}
+
+
+}
---
+++ ThePiaretsDenGame/Assets/_Scripts/MonoBehaviours/MONO_Menus.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MONO_Menus : MonoBehaviour {
+
+	public enum menu {main, paus, settings};
+
+	private menu latestMenu;
+	[SerializeField]
+	private GameObject mainMenu;
+	[SerializeField]
+	private GameObject pausMenu;
+	[SerializeField]
+	private GameObject settingsMenu;
+
+	void Start()
+	{
+		latestMenu = menu.paus; 	//paus menu is the default menu
+	}
+
+	public void StartNewGame()
+	{
+		
+	}
+
+
+	public void OpenMenu()
+	{
+		switch (latestMenu) {
+		case menu.main:
+			mainMenu.SetActive (true);
+			break;
+
+		case menu.paus:
+			pausMenu.SetActive (true);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	/// <summary>
+	/// Quit game.
+	/// If we're in the unity editor, exit play mode.
+	/// If we're running the application, quit the application.
+	/// </summary>
+	public void Quit()
+	{
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#else
+			Application.Quit();
+		#endif
+	}
+
+}
