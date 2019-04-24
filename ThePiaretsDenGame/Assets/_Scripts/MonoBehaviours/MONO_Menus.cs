3ef16331a0033377d96461da29acbe472b36a68c
Sara <sara.uvalic@hotmail.com>
2019-04-24 11:31:49 +0200

    Menus basically working, needs settings and save handling for everything to work

--- ThePiaretsDenGame/Assets/_Scripts/MonoBehaviours/MONO_Menus.cs
+++ ThePiaretsDenGame/Assets/_Scripts/MonoBehaviours/MONO_Menus.cs
@@ -7,6 +7,7 @@ public class MONO_Menus : MonoBehaviour {
 
 	public enum menu {main, paus, settings};
 
+
 	private menu latestMenu;
 	[SerializeField]
 	private GameObject mainMenu;
@@ -20,12 +21,38 @@ public class MONO_Menus : MonoBehaviour {
 		latestMenu = menu.paus; 	//paus menu is the default menu
 	}
 
+
 	public void StartNewGame()
 	{
 		
 	}
 
+	public void LoadLatestGame()
+	{
+		
+	}
 
+
+
+	public void ChangeLatestMenu(GameObject menuObject)
+	{
+		if (menuObject.name == "Main") {
+			latestMenu = menu.main;
+		} 
+		else if (menuObject.name == "Paus")
+		{
+			latestMenu = menu.paus;	
+		}
+
+	}
+
+	/// <summary>
+	/// Opens the menu. 
+	/// This is based on which menu that was opened most recently, so 
+	/// that you can go back to whatever menu you came from when going 
+	/// back from, ex, the settings menu. The latest menu is set to be the 
+	/// paus menu by default, since this is the menu you open from in game.
+	/// </summary>
 	public void OpenMenu()
 	{
 		switch (latestMenu) {
