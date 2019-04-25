3d64fde47a530326f17fdd8be7467f2eb48dce28
Sebastian <sebbe.carlsson@gmail.com>
2019-04-25 09:33:13 +0200

    wait a sec

--- ThePiaretsDenGame/Assets/_Scripts/ScriptableObjects/Interaction/Reactions/ImmediateReactions/SOBJ_TextDialog.cs
+++ ThePiaretsDenGame/Assets/_Scripts/ScriptableObjects/Interaction/Reactions/ImmediateReactions/SOBJ_TextDialog.cs
@@ -3,10 +3,11 @@
 public class SOBJ_TextDialog : SOBJ_Reaction
 {
 	public Fungus.Flowchart flowchart;       // The gameobject to be turned on or off.
+	public string interactionBlock;
 
 	protected override void ImmediateReaction()
 	{
-		flowchart.ExecuteBlock ("Start");
+		flowchart.ExecuteBlock (interactionBlock);
 	}
 }
 
