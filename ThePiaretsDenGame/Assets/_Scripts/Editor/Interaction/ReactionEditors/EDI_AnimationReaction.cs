4ec9eb5e93b7cf6360a636685879c92390fa56fb
Sebastian <sebbe.carlsson@gmail.com>
2019-04-26 16:11:53 +0200

    Animation Reaction

---
+++ ThePiaretsDenGame/Assets/_Scripts/Editor/Interaction/ReactionEditors/EDI_AnimationReaction.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+[CustomEditor(typeof(SOBJ_AnimationReaction))]
+public class EDI_AnimationReaction : EDI_Reaction {
+
+
+	protected override string GetFoldoutLabel()
+	{
+		return "AnimationReaction";
+	}
+}
