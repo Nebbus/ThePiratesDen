1244c4da8421d2ef444f1767334f23d9ce2dedf9
Franz <a17frajo@student.his.se>
2019-04-29 15:07:15 +0200

    Fungerande muse bundarys
    Tack VIgfurs

--- ThePiaretsDenGame/Assets/_Scripts/MonoBehaviours/MouseCursor/MONO_CustomMouseCursor.cs
+++ ThePiaretsDenGame/Assets/_Scripts/MonoBehaviours/MouseCursor/MONO_CustomMouseCursor.cs
@@ -10,15 +10,18 @@ public class MONO_CustomMouseCursor : MonoBehaviour {
     public bool       UsingKeyboard = false;
     public GameObject CustomCursor;
 
-    public Canvas canvas;
-    public RectTransform fadeImage;
-
-
     [SerializeField]
     private  float  CursorSpeed = 4;
+    [SerializeField]
+    [Tooltip("Pixels distans the muse points out on the right side and on the bottom")]
+    private float pointerSafeDist = 5;
+
     private RectTransform   CustomCursorTransform;
     private MONO_Menus      Menus;
-   
+    public bool useMosePosition = true;
+
+  
+    
 
     void Awake()
     {
@@ -29,8 +32,8 @@ public class MONO_CustomMouseCursor : MonoBehaviour {
 
     private void Start()
     {
-        Cursor.visible = false;
-        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+
     }
 
     void Update () {
@@ -45,46 +48,55 @@ public class MONO_CustomMouseCursor : MonoBehaviour {
         {
             // get movent of key 
             //using muse position
-            float x = UsingKeyboard ? Input.GetAxis("Horizontal") : Input.mousePosition.x;// Input.GetAxis("Mouse X");
-            float y = UsingKeyboard ? Input.GetAxis("Vertical") : Input.mousePosition.y;// Input.GetAxis("Mouse Y");
-            x = UsingKeyboard ? CustomCursorTransform.anchoredPosition.x + (x * CursorSpeed * Time.deltaTime) : x;
-            y = UsingKeyboard ? CustomCursorTransform.anchoredPosition.y + (y * CursorSpeed * Time.deltaTime) : y;
+            //float x, y;
+            //if (useMosePosition)
+            //{
+            //    //// using axes
+            //    if (Cursor.lockState == CursorLockMode.Locked)
+            //    {
+            //        Cursor.lockState = CursorLockMode.None;
+            //    }
+            //     //x = UsingKeyboard ? Input.GetAxis("Horizontal") : Input.mousePosition.x;
+            //     //y = UsingKeyboard ? Input.GetAxis("Vertical") : Input.mousePosition.y;
+            //     //x = UsingKeyboard ? CustomCursorTransform.anchoredPosition.x + (x * CursorSpeed * Time.deltaTime) : x;
+            //     //y = UsingKeyboard ? CustomCursorTransform.anchoredPosition.y + (y * CursorSpeed * Time.deltaTime) : y;
 
-            // using axes
-            //float x = UsingKeyboard ? Input.GetAxis("Horizontal") :  Input.GetAxis("Mouse X");
-            //float y = UsingKeyboard ? Input.GetAxis("Vertical") : Input.GetAxis("Mouse Y");
-            //x = CustomCursorTransform.anchoredPosition.x + (x * CursorSpeed * Time.deltaTime);
-            //y = CustomCursorTransform.anchoredPosition.y + (y * CursorSpeed * Time.deltaTime);
+            //}
+            //else
+            //{
+            //// using axes
+
+            //}
+
+            //Locks the cursor 
+            if (Cursor.lockState != CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+
+            // Gets the movment direction
+            float x = UsingKeyboard ? Input.GetAxis("Horizontal") : Input.GetAxis("Mouse X");
+            float y = UsingKeyboard ? Input.GetAxis("Vertical")   : Input.GetAxis("Mouse Y");
+
+            // Adds the momvent
+            float xTemp = CustomCursorTransform.anchoredPosition.x + (x * CursorSpeed * Time.deltaTime);
+            float yTemp = CustomCursorTransform.anchoredPosition.y + (y * CursorSpeed * Time.deltaTime);
 
 
-            float xTemp = (x );
-            float yTemp = (y );
+            // Bounds code Vigfus
+            float refHeight = 1080;// the referens hight
+            float refWidth = ((float)Screen.width / (float)Screen.height) * refHeight;// the referens whidt that works with diffrent higts
 
 
-            Vector3 pos   = new Vector3(xTemp, yTemp, 0f);
-            Vector3 image = new Vector3(CustomCursorTransform.rect.width, CustomCursorTransform.rect.height, 0f);
+            float minX = 0f;
+            float minY = 0f + pointerSafeDist;
+            float maxX = refWidth - pointerSafeDist; 
+            float maxY = refHeight;
 
-            // pos = Camera.main.ScreenToViewportPoint(pos);
-            //image = Camera.main.ScreenToViewportPoint(image);
-            //pos.x = Mathf.Clamp(pos.x, 0f, 1f + image.x);
-            //pos.y = Mathf.Clamp(pos.y, 0f+ image.y, 1f + image.y);
-            
+            xTemp = Mathf.Clamp(xTemp, minX, maxX );
+            yTemp = Mathf.Clamp(yTemp, minY , maxY);
 
-            float w = CustomCursorTransform.rect.width;
-            float h = CustomCursorTransform.rect.height;
-            float xx = 0f;// canvas.pixelRect.xMin;//fadeImage.anchorMin.x;// screenBounds.x + w;
-            float XX = Screen.width; //canvas.pixelRect.xMax;// fadeImage.anchorMax.x;// screenBounds.x * -1 - w;
-            float yy = 0f;// canvas.pixelRect.yMin;//fadeImage.anchorMin.y;// screenBounds.y + h;
-            float YY = Screen.height; //canvas.pixelRect.yMax;//fadeImage.anchorMax.y;// screenBounds.y * -1 - h;
-            Debug.DrawLine(new Vector3(xx, yy, 0f), new Vector3(XX, yy, 0f), Color.green);
-            Debug.DrawLine(new Vector3(XX, yy, 0f), new Vector3(XX, YY, 0f), Color.red);
-            Debug.DrawLine(new Vector3(XX, YY, 0f), new Vector3(xx, YY, 0f), Color.blue);
-            Debug.DrawLine(new Vector3(xx, YY, 0f), new Vector3(xx, yy, 0f), Color.magenta);
-
-            pos.x = Mathf.Clamp(pos.x, xx, XX+ image.x);
-            pos.y = Mathf.Clamp(pos.y, yy + image.y, YY+ image.y);
-            //pos = Camera.main.ViewportToScreenPoint(pos);
-            CustomCursorTransform.anchoredPosition = new Vector2(pos.x, pos.y);
+            CustomCursorTransform.anchoredPosition = new Vector2(xTemp, yTemp);
  
         }
 
