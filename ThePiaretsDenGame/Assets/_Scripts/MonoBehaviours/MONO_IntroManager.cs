b41d405ae40cbba141599dac78117e31ec6aa744
Sara Uvalic <sara.uvalic@hotmail.com>
2019-04-30 18:14:13 +0200

    Game starts from the main menu

---
+++ ThePiaretsDenGame/Assets/_Scripts/MonoBehaviours/MONO_IntroManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MONO_IntroManager : MonoBehaviour {
+	private MONO_SceneManager sceneManager;
+	private MONO_Menus menuManager;
+	public GameObject mainMenu;
+
+	private MONO_MenuSelect temp;
+
+	void Awake()
+	{
+		sceneManager = FindObjectOfType<MONO_SceneManager> ();
+		menuManager = FindObjectOfType <MONO_Menus> ();
+	}
+
+
+	void Start () {
+		
+	}
+
+	void LateStart()
+	{
+		menuManager.ChangeLatestMenu (mainMenu);
+		//menuManager.OpenMenu ();
+	}
+}
