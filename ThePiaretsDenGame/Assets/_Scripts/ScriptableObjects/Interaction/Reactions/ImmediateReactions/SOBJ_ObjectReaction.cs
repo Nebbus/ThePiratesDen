65d36c0455788c0700c61fb266819b91860807e6
Sebastian <sebbe.carlsson@gmail.com>
2019-05-02 16:51:06 +0200

    Cleanup on scene1

--- ThePiaretsDenGame/Assets/_Scripts/ScriptableObjects/Interaction/Reactions/ImmediateReactions/SOBJ_ObjectReaction.cs
+++ ThePiaretsDenGame/Assets/_Scripts/ScriptableObjects/Interaction/Reactions/ImmediateReactions/SOBJ_ObjectReaction.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class SOBJ_ObjectReaction : SOBJ_Reaction
+public class SOBJ_ObjectReaction : SOBJ_DelayedReaction
 {
     public GameObject gameObject;       // The gameobject to be turned on or off.
     public bool activeState;            // The state that the gameobject will be in after the Reaction.
