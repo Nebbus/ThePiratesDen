e64c408ca00d6bf388286ab825c6924ab8f07897
Sara Uvalic <sara.uvalic@hotmail.com>
2019-05-14 17:18:38 +0200

    SayDialog prefabs and some work on FadeObject

--- SecretOfTheOldShip/Assets/_Scripts/MonoBehaviours/Misc/MONO_FadeObject.cs
+++ SecretOfTheOldShip/Assets/_Scripts/MonoBehaviours/Misc/MONO_FadeObject.cs
@@ -4,39 +4,86 @@ using UnityEngine;
 
 public class MONO_FadeObject : MonoBehaviour {
 	public float fadeDuration = 1.0f;
-	[Tooltip("Mesh renderer of the object to fade")]
-	public SkinnedMeshRenderer meshRenderer;
+	[Tooltip("Mesh rendererers of the object to fade")]
+	public MeshRenderer[] meshRenderers;
+	[Tooltip("Skinned mesh rendererers of the object to fade")]
+	public SkinnedMeshRenderer[] skinnedMeshRenderers;
 
-	private Color startColor;
-	private Color targetColor;
-	private float objectAlpha;
+
+	private Color[] startColor;
+	private Color[] skinnedStartColor;
+	private Color[] targetColor;
+	private Color[] skinnedTargetColor;
+	private float currentAlpha;
 	private float targetAlpha;
 	private bool isFading;
 
+	void Start()
+	{
+		startColor = new Color[meshRenderers.Length];
+		skinnedStartColor = new Color[skinnedMeshRenderers.Length];
+	}
 
 
 	public void StartFade(float newAlpha)
 	{
 		targetAlpha = newAlpha;
-		startColor = meshRenderer.material.color;
-		targetColor = startColor;
-		targetColor.a = targetAlpha;
-		objectAlpha = startColor.a;
+		currentAlpha = (newAlpha + 1) % 1;
+		if (meshRenderers == null && skinnedMeshRenderers == null) 
+		{
+			Debug.LogError ("You cannot fade an object without at least one mesh renderer or skinned mesh renderer.");
+			return;
+		}
+		for (int i = 0; i < meshRenderers.Length; i++) 
+		{
+			startColor[i] = meshRenderers [i].material.color;
+			targetColor [i] = startColor [i];
+			targetColor [i].a = targetAlpha;
+		}
+		for (int i = 0; i < skinnedMeshRenderers.Length; i++) 
+		{
+			skinnedStartColor[i] = skinnedMeshRenderers [i].material.color;
+			targetColor [i] = startColor [i];
+			targetColor [i].a = targetAlpha;
+		}
+
 		isFading = true;
 	}
 
 
 	void Update()
 	{
-		float fadeSpeed = Mathf.Abs (objectAlpha - targetAlpha) / fadeDuration;
+		float fadeSpeed = (Mathf.Abs (currentAlpha - targetAlpha)) / fadeDuration;
 
 		// While the CanvasGroup hasn't reached the final alpha yet...
-		while (!Mathf.Approximately (objectAlpha, targetAlpha))
+		while (!Mathf.Approximately (currentAlpha, targetAlpha))
 		{
-			Color objectColor = Color.Lerp (startColor, targetColor, fadeSpeed);
-			objectAlpha = objectColor.a;
-			meshRenderer.material.color = objectColor;
+			for (int i = 0; i < meshRenderers.Length; i++)
+			{
+				Color objectColor = meshRenderers [i].material.color;
+				objectColor = Color.Lerp (objectColor, targetColor[i], fadeSpeed);
+				meshRenderers [i].material.color = objectColor;
+			}
+			for (int i = 0; i < skinnedMeshRenderers.Length; i++)
+			{
+				skinnedMeshRenderers[i].material.color = Color.Lerp (skinnedMeshRenderers[i].material.color, targetColor[i], fadeSpeed);
+			}
+
+			if (meshRenderers [0] != null)
+			{
+				currentAlpha = meshRenderers [0].material.color.a;
+			} 
+			else if (skinnedMeshRenderers [0] != null) 
+			{
+				currentAlpha = skinnedMeshRenderers [0].material.color.a;
+			} 
+			else
+			{
+				Debug.Log ("There is no mesh renderer or skinned mesh renderer to be faded.");
+			}
+
 		}
+
 		isFading = false;
 	}
 
