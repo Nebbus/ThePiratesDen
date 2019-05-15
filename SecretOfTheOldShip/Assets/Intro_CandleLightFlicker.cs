0650bbae0006bc3fdfbc0388c334e15ef37d4504
Emma <emma_sw@hotmail.com>
2019-05-15 16:11:54 +0200

    Börjat fixa introljuset

---
+++ SecretOfTheOldShip/Assets/Intro_CandleLightFlicker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro_CandleLightFlicker : MonoBehaviour {
+    public Random randomRange;
+    public Light lt;
+    float duration = 1.0f;
+    Color color0 = Color.red;
+    Color color1 = Color.blue;
+    void Start () {
+        lt = GetComponent<Light>();
+	}
+
+    void Update() {
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        lt.color = Color.Lerp(color0, color1, t);
+    }
+
+
+
+}
