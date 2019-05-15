9119f8205e8d792658e2b2562c250c6241a65157
Sebastian <sebbe.carlsson@gmail.com>
2019-05-15 10:16:41 +0200

    Added FMOD command in fungus

---
+++ SecretOfTheOldShip/Assets/_Resources/Fungus/Scripts/Commands/PlayFMODSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+using FMOD;
+
+[CommandInfo("FMOD", "Play FMOD Sound", "Plays an FMOD sound")]
+public class PlayFMODSound : Command 
+{
+	[FMODUnity.EventRef]
+	public string pathToFMODSound;
+
+	public override void OnEnter()
+	{
+		FMODUnity.RuntimeManager.PlayOneShot(pathToFMODSound);
+		Continue();
+	}
+
+	public override string GetSummary()
+	{
+		return "FMODSound";
+	}
+
+	public override Color GetButtonColor ()
+	{
+		return Color.yellow;
+	}
+}
