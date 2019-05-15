1e6f4324ddcb77a2dc7be28a2b9ab6ea0c1a33a8
Sara Uvalic <sara.uvalic@hotmail.com>
2019-05-15 16:50:15 +0200

    Put the change cursor sprite function in a alonestanding script

---
+++ SecretOfTheOldShip/Assets/_Scripts/MonoBehaviours/MouseCursor/MONO_CursorSprite.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MONO_CursorSprite : MonoBehaviour {
+
+
+	public SpriteRenderer cursorSpriteRenderer;
+	public cursorSprite[] cursorSprites = new cursorSprite[0];
+
+	/// <summary>
+	/// Compares the tag of object being hovered over with the tag of the cursorSprite. 
+	/// If they match, the sprite of the cursor changes to the sprite of the cursorSprite.
+	/// </summary>
+	/// <param name="objectTag">Tag of the object currently being hovered over.</param>
+	private void ChangeCursorSprite(string objectTag)
+	{
+		for (int i = 0; i < cursorSprites.Length; i++) 
+		{
+			if (cursorSprites [i].tag == objectTag) 
+			{
+				cursorSpriteRenderer.sprite = cursorSprites [i].sprite;
+				return;
+			}
+
+		}
+		Debug.LogError ("No sprite with matching tag found");
+	}
+}
+
+public class cursorSprite 
+{
+	public string tag;
+	public Sprite sprite;
+}
