6f46632b3fa58c79f3e62c4d9d89b814463205e5
Sara Uvalic <sara.uvalic@hotmail.com>
2019-05-20 14:58:00 +0200

    Get Condition State Reaction
    only booleans

---
+++ SecretOfTheOldShip/Assets/_Scripts/Editor/Interaction/ReactionEditors/EDI_GetConditionStateReaction.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(SOBJ_GetConditionStateReaction))]
+public class EDI_GetConditionStateReaction : EDI_Reaction
+{
+	protected override string GetFoldoutLabel()
+	{
+		return "Get Condition State Reaction";
+	}
+}
---
+++ SecretOfTheOldShip/Assets/_Scripts/ScriptableObjects/Interaction/Reactions/ImmediateReactions/SOBJ_GetConditionStateReaction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SOBJ_GetConditionStateReaction : SOBJ_Reaction
+{
+	[Tooltip("The condition to be checked")]
+	public SOBJ_ConditionAdvanced condition;
+
+	[Space]
+	[Tooltip("Fungus Flowchart containing variable to be set to condition state.")]
+	public Fungus.Flowchart destinationFlowchart;
+	[Tooltip("Name of the variable keeping condition state.")]
+	public string variableName;
+
+	private bool conditionState;
+
+	protected override void ImmediateReaction()
+	{
+		conditionState = condition.satisfied;
+		destinationFlowchart.SetBooleanVariable (variableName, conditionState);
+	}
+}
