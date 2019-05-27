8d8b8cd2a2c5d53b279050d05188ef95a1882392
Sara Uvalic <sara.uvalic@hotmail.com>
2019-05-27 18:17:52 +0200

    small outro fix

---
+++ SecretOfTheOldShip/Assets/_Resources/Fungus/Scripts/Commands/GetVideoLength.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Fungus
+{
+	[CommandInfo("Video",
+		"Get Video Length",
+		"Gets the length of a video clip and puts it in a float variable.")]
+	public class GetVideoLength : Command 
+	{
+		public UnityEngine.Video.VideoPlayer videoPlayer;
+
+		[Tooltip("Float variable to store the length of the video clip in.")]
+		[VariableProperty(typeof(FloatVariable))]
+		[SerializeField] protected FloatVariable variable;
+
+		private UnityEngine.Video.VideoClip videoClip;
+
+		private SetVariable setVariableCommand;
+
+		public override void OnEnter() 
+		{
+			LengthToVariable ();
+
+			Continue();
+		}
+
+		private void LengthToVariable (){
+			videoClip = videoPlayer.clip;
+			setVariableCommand.SetNewVariable (variable, SetOperator.Assign, videoClip.length);
+			setVariableCommand.OnEnter ();
+		}
+	}
+}
