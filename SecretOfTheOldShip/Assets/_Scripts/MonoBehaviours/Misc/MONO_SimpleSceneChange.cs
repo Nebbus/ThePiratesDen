41422039f9d90c77283e3a916adac0c66dadb467
Sara Uvalic <sara.uvalic@hotmail.com>
2019-05-31 19:57:49 +0200

    splash screen scene stuff

---
+++ SecretOfTheOldShip/Assets/_Scripts/MonoBehaviours/Misc/MONO_SimpleSceneChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MONO_SimpleSceneChange : MonoBehaviour {
+	public string SceneToLoad;
+
+	void Start()
+	{
+		SceneManager.LoadScene (SceneToLoad);
+	}
+}
