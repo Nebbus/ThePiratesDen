db4a9c7001fbae0401e9a4e80a8e0502eec10159
Sebastian <sebbe.carlsson@gmail.com>
2019-06-03 10:16:38 +0200

    ChangeTexReaction

---
+++ SecretOfTheOldShip/Assets/_Scripts/Editor/Interaction/ReactionEditors/EDI_ChangeTextureReaction.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+[CustomEditor(typeof(SOBJ_ChangeTextureReaction))]
+public class EDI_ChangeTextureReaction : EDI_Reaction {
+
+
+	protected override string GetFoldoutLabel()
+	{
+		return "ChangeTexture";
+	}
+}
---
+++ SecretOfTheOldShip/Assets/_Scripts/ScriptableObjects/Interaction/Reactions/DelayedReactions/SOBJ_ChangeTextureReaction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SOBJ_ChangeTextureReaction : SOBJ_DelayedReaction
+{
+	public Material target;
+	public GameObject gameObject;
+
+
+
+    protected override void ImmediateReaction()
+	{
+		gameObject.GetComponent<Renderer> ().material.mainTexture = target;
+	}
+}
+
