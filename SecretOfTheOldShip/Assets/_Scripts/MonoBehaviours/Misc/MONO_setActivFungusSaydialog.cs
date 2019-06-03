286cd3ec8c16103c633172e21818ddfdf1011e0f
Franz <a17frajo@student.his.se>
2019-06-03 20:46:52 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ SecretOfTheOldShip/Assets/_Scripts/MonoBehaviours/Misc/MONO_setActivFungusSaydialog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MONO_setActivFungusSaydialog : MonoBehaviour {
+
+    public Fungus.SayDialog lastBox;
+
+   public void setMeActive()
+    {
+        Fungus.SayDialog.ActiveSayDialog = lastBox;
+    }
+}
