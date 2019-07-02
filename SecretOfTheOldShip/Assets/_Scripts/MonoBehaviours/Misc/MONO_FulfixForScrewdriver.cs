2958dda1339a89e689e2f4109f74d8bb3e048e8c
Sebastian <sebbe.carlsson@gmail.com>
2019-07-02 20:42:54 +0200

    Created a way for screwdriver to be used from inside the puzzlescene

---
+++ SecretOfTheOldShip/Assets/_Scripts/MonoBehaviours/Misc/MONO_FulfixForScrewdriver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MONO_FulfixForScrewdriver : MonoBehaviour {
+
+	public GameObject screwdriverButton;
+	public Fungus.Flowchart variableFlowchart1;
+
+	// Use this for initialization
+	void Start () {
+		if (variableFlowchart1.GetBooleanVariable("Screwdriver") == true) 
+		{
+			screwdriverButton.SetActive (true);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
